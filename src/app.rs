55a51fc4e8394834b953c4117fd22afe36a2c906
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-12-24 16:13:10 +0100

    wip

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,7 @@ use leptos_router::*;
 use serde::{Deserialize, Serialize};
 
 #[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct TodoItem {
     id: u32,
     done: bool,
@@ -68,18 +68,20 @@ pub async fn get_todos(search: String) -> Result<Vec<TodoItem>, ServerFnError> {
 }
 
 #[server(AddTodo, "/api")]
-pub async fn add_todo(todo: String) -> Result<(), ServerFnError> {
+pub async fn add_todo(todo: String) -> Result<TodoItem, ServerFnError> {
     let pool = db().await?;
 
     // fake API delay
     std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    match sqlx::query("INSERT INTO todos (task, done) VALUES ($1, false)")
-        .bind(todo)
-        .execute(&pool)
-        .await
+    match sqlx::query_as::<_, TodoItem>(
+        "INSERT INTO todos (task, done) VALUES ($1, false) RETURNING *",
+    )
+    .bind(todo)
+    .fetch_one(&pool)
+    .await
     {
-        Ok(_) => Ok(()),
+        Ok(todo) => Ok(todo),
         Err(e) => Err(ServerFnError::ServerError(e.to_string())),
     }
 }
@@ -109,17 +111,17 @@ pub async fn delete_all() -> Result<(), ServerFnError> {
 }
 
 #[server(ToggleTodo, "/api")]
-pub async fn toggle_todo(id: u32) -> Result<(), ServerFnError> {
+pub async fn toggle_todo(id: u32) -> Result<TodoItem, ServerFnError> {
     let pool = db().await?;
 
-    match sqlx::query(
-        "UPDATE todos SET done = (CASE WHEN done = false THEN true ELSE false END) WHERE id = $1",
+    match sqlx::query_as::<_, TodoItem>(
+        "UPDATE todos SET done = (CASE WHEN done = false THEN true ELSE false END) WHERE id = ? RETURNING *",
     )
     .bind(id)
-    .execute(&pool)
+    .fetch_one(&pool)
     .await
     {
-        Ok(_) => Ok(()),
+        Ok(todo) => Ok(todo),
         Err(e) => Err(ServerFnError::ServerError(e.to_string())),
     }
 }
@@ -191,17 +193,68 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
+    // get existing todos from server
+    let existing_todos = create_resource(|| (), |_| async move { get_todos("".to_string()).await });
+
+    // Submit changes to server
     let add_todo = create_server_action::<AddTodo>();
     let delete_todo = create_server_action::<DeleteTodo>();
     let toggle_todo = create_server_action::<ToggleTodo>();
     let mark_all_done = create_server_action::<MarkAllDone>();
     let mark_all_undone = create_server_action::<MarkAllUndone>();
     let delete_all = create_server_action::<DeleteAll>();
+
+    // Local interaction
     let (search, set_search) = create_signal("".to_string());
 
     // list of todos is loaded from the server in reaction to changes
-    let todos = create_resource(move || search.get(), get_todos);
+    //let todos = create_resource(move || search.get(), get_todos);
 
+    // Merge todos from changes and interactions into one signal
+    let (todos, set_todos) = create_signal::<Vec<RwSignal<TodoItem>>>(vec![]);
+    create_effect(move |_| {
+        if let Some(Ok(exiting_todos)) = existing_todos.get() {
+            set_todos.update(|todos| {
+                todos.extend(exiting_todos.into_iter().map(|todo| create_rw_signal(todo)))
+            });
+        }
+    });
+    create_effect(move |_| {
+        if let Some(Ok(todo)) = add_todo.value().get() {
+            set_todos.update(|todos| todos.push(create_rw_signal(todo)));
+        };
+    });
+    create_effect(move |_| {
+        if let Some(Ok(toggled)) = toggle_todo.value().get() {
+            set_todos.update(|todos| {
+                for todo in todos {
+                    if todo.get().id == toggled.id {
+                        todo.set(toggled);
+                        logging::log!("updated todo");
+                        break;
+                    }
+                }
+            });
+        };
+    });
+    /*
+        let todos = Signal::derive(move || {
+            if let Some(Ok(exiting_todos)) = exiting_todos.get() {
+                set_todos.update(|todos| {
+                    todos.extend(exiting_todos.into_iter().map(|todo| create_rw_signal(todo)))
+                });
+            }
+
+            match add_todo.value().get() {
+                Some(Ok(todo)) => set_todos.update(|todos| todos.push(create_rw_signal(todo))),
+                _ => (),
+            };
+            get_todos()
+                .into_iter()
+                .filter(|todo: &RwSignal<TodoItem>| todo.get().task.contains(&search()))
+                .collect::<Vec<RwSignal<TodoItem>>>()
+        });
+    */
     view! {
         <Topbar set_search/>
         <div class="container mt-3">
@@ -248,20 +301,24 @@ fn Topbar(set_search: WriteSignal<String>) -> impl IntoView {
 
 #[component]
 fn Todolist(
-    todos: Resource<String, Result<Vec<TodoItem>, ServerFnError>>,
+    //todos: Resource<String, Result<Vec<TodoItem>, ServerFnError>>,
+    todos: ReadSignal<Vec<RwSignal<TodoItem>>>,
     delete_todo: Action<DeleteTodo, Result<(), leptos::ServerFnError>>,
-    toggle_todo: Action<ToggleTodo, Result<(), leptos::ServerFnError>>,
+    toggle_todo: Action<ToggleTodo, Result<TodoItem, leptos::ServerFnError>>,
 ) -> impl IntoView {
     view! {
         <div>
             <Suspense fallback=move || view! { <p class="text-muted">"Loading..."</p> }>
-                {move || match todos.get() {
+            /*
+                {move || match todos() {
                     None => view! { <p class="text-muted">"No data"</p> }.into_view(),
                     Some(result) => match result {
                         Err(e) => view! { <p class="text-danger">"Error loading: " {e.to_string()}</p> }.into_view(),
                         Ok(data) => view! { <ShowTodos data delete_todo toggle_todo/> }.into_view(),
                     }
                 }}
+            */
+            <ShowTodos todos delete_todo toggle_todo/>
             </Suspense>
         </div>
     }
@@ -269,18 +326,19 @@ fn Todolist(
 
 #[component]
 fn ShowTodos(
-    data: Vec<TodoItem>,
+    todos: ReadSignal<Vec<RwSignal<TodoItem>>>,
     delete_todo: Action<DeleteTodo, Result<(), leptos::ServerFnError>>,
-    toggle_todo: Action<ToggleTodo, Result<(), leptos::ServerFnError>>,
+    toggle_todo: Action<ToggleTodo, Result<TodoItem, leptos::ServerFnError>>,
 ) -> impl IntoView {
     view! {
         <For
             // a function that returns the items we're iterating over; a signal is fine
-            each=move || data.clone().into_iter()
+            each=move || todos.get()
             // a unique key for each item
-            key=|item| item.id
+            key=|item| item.get().id
             // renders each item to a view
             children=move |item| {
+                let item = item.get();
                 let toggle_class = format!("btn btn-sm border-0 bi {}",
                     if item.done {
                         "bi-check-square-fill btn-outline-success"
@@ -314,7 +372,7 @@ fn ShowTodos(
 }
 
 #[component]
-fn Todoadd(add_todo: Action<AddTodo, Result<(), leptos::ServerFnError>>) -> impl IntoView {
+fn Todoadd(add_todo: Action<AddTodo, Result<TodoItem, leptos::ServerFnError>>) -> impl IntoView {
     view! {
         <ActionForm action=add_todo>
             <div class="input-group">
